2ac6c62c7f2818b8a0f773f0c1af1b2d9ec0c2af
joparras <121075042+joparras@users.noreply.github.com>
2022-12-26 15:05:06 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CL3_POLICARPIO_PARRAS_JOSE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtFechaInicio = new System.Windows.Forms.DateTimePicker();
+            this.dtFechaFinal = new System.Windows.Forms.DateTimePicker();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            this.dgCompras = new System.Windows.Forms.DataGridView();
+            this.cboProveedor = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCompras)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(181, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REPORTES DE COMPRAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha Inicio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(252, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fecha Final";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(491, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Proveedor";
+            // 
+            // dtFechaInicio
+            // 
+            this.dtFechaInicio.Location = new System.Drawing.Point(109, 53);
+            this.dtFechaInicio.Name = "dtFechaInicio";
+            this.dtFechaInicio.Size = new System.Drawing.Size(137, 20);
+            this.dtFechaInicio.TabIndex = 4;
+            // 
+            // dtFechaFinal
+            // 
+            this.dtFechaFinal.Location = new System.Drawing.Point(346, 53);
+            this.dtFechaFinal.Name = "dtFechaFinal";
+            this.dtFechaFinal.Size = new System.Drawing.Size(139, 20);
+            this.dtFechaFinal.TabIndex = 5;
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsulta.Location = new System.Drawing.Point(695, 50);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(93, 23);
+            this.btnConsulta.TabIndex = 7;
+            this.btnConsulta.Text = "CONSULTA";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            this.btnConsulta.Click += new System.EventHandler(this.btnConsulta_Click);
+            // 
+            // dgCompras
+            // 
+            this.dgCompras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCompras.Location = new System.Drawing.Point(15, 105);
+            this.dgCompras.Name = "dgCompras";
+            this.dgCompras.Size = new System.Drawing.Size(773, 232);
+            this.dgCompras.TabIndex = 8;
+            // 
+            // cboProveedor
+            // 
+            this.cboProveedor.FormattingEnabled = true;
+            this.cboProveedor.Location = new System.Drawing.Point(577, 52);
+            this.cboProveedor.Name = "cboProveedor";
+            this.cboProveedor.Size = new System.Drawing.Size(98, 21);
+            this.cboProveedor.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboProveedor);
+            this.Controls.Add(this.dgCompras);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.dtFechaFinal);
+            this.Controls.Add(this.dtFechaInicio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgCompras)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtFechaInicio;
+        private System.Windows.Forms.DateTimePicker dtFechaFinal;
+        private System.Windows.Forms.Button btnConsulta;
+        private System.Windows.Forms.DataGridView dgCompras;
+        private System.Windows.Forms.ComboBox cboProveedor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration; 
+
+
+
+namespace CL3_POLICARPIO_PARRAS_JOSE
+{
+    public partial class Form1 : Form
+    {
+        String cadena = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+        private DataTable GetComprasByFechasProveedor(DateTime fechaInicio, DateTime fechaFin, int idProveedor)
+        {
+            SqlConnection connection = new SqlConnection(cadena);
+            string sentenciaSQL = "EXEC sp_GetComprasByFechasProveedor @prmdatFechaInicio, @prmdatFechaFin,@prmintIdProveedor";
+
+
+            SqlCommand sqlCommand = new SqlCommand(sentenciaSQL, connection);
+            sqlCommand.Parameters.AddWithValue("@prmdatFechaInicio", fechaInicio);
+            sqlCommand.Parameters.AddWithValue("@prmdatFechaFin", fechaFin);
+            sqlCommand.Parameters.AddWithValue("@prmintIdProveedor", idProveedor);
+
+            connection.Open();
+
+            SqlDataReader dr = sqlCommand.ExecuteReader();
+
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("IdOrdenCompra");
+            dataTable.Columns.Add("Codigo");
+            dataTable.Columns.Add("FechaEmision");
+            dataTable.Columns.Add("idProveedor");
+            dataTable.Columns.Add("razonSocial");
+            dataTable.Columns.Add("total");
+
+
+            while (dr.Read())
+            {
+                DataRow fila = dataTable.NewRow();
+
+                fila[0] = dr.GetInt32(0);
+                fila[1] = dr.GetString(1);
+                fila[2] = dr.GetDateTime(2);
+                fila[3] = dr.GetInt32(3);
+                fila[4] = dr.GetString(4);
+                fila[5] = dr.GetDecimal(5);
+
+                dataTable.Rows.Add(fila);
+
+            }
+            connection.Close();
+            return dataTable; 
+        }
+
+        private DataTable GetProveedores()
+        {
+            SqlConnection connection = new SqlConnection(cadena);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("sp_GetProveedores", connection);
+            dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            return dataTable;
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            cboProveedor.DataSource = GetProveedores();
+            cboProveedor.DisplayMember = "razonSocial";
+            cboProveedor.ValueMember = "idProveedor";
+     
+        }
+
+        private void btnConsulta_Click(object sender, EventArgs e)
+        {
+           
+          dgCompras.DataSource = GetComprasByFechasProveedor(dtFechaInicio.Value, dtFechaFinal.Value, (int)cboProveedor.SelectedValue); 
+            
+        }
+    }
+}
