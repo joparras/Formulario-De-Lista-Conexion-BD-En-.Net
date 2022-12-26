e2700034b413b89b426ce67b1974b8afe824f005
joparras <121075042+joparras@users.noreply.github.com>
2022-12-26 15:38:33 -0500

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace CL3_POLICARPIO_PARRAS_JOSE
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.dgProducto = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProducto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(302, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTRO DE PRODUCTOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CATEGORIA:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "NOMBRE:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "PRECIO:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(53, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "STOCK:";
+            // 
+            // cboCategoria
+            // 
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Location = new System.Drawing.Point(136, 88);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(100, 21);
+            this.cboCategoria.TabIndex = 6;
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(136, 61);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 7;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(136, 117);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(136, 143);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 9;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(136, 173);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 20);
+            this.txtStock.TabIndex = 10;
+            // 
+            // dgProducto
+            // 
+            this.dgProducto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProducto.Location = new System.Drawing.Point(254, 61);
+            this.dgProducto.Name = "dgProducto";
+            this.dgProducto.Size = new System.Drawing.Size(534, 187);
+            this.dgProducto.TabIndex = 11;
+            this.dgProducto.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgProducto_CellDoubleClick);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(233, 265);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(85, 23);
+            this.btnAgregar.TabIndex = 12;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(334, 265);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(96, 23);
+            this.btnActualizar.TabIndex = 13;
+            this.btnActualizar.Text = "ACTUALIZAR";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(450, 265);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgProducto);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.cboCategoria);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dgProducto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.DataGridView dgProducto;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace CL3_POLICARPIO_PARRAS_JOSE
+{
+    public partial class Form2 : Form
+    {
+        static string cadena = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+        private DataTable GetProductos()
+        {
+            SqlConnection connection = new SqlConnection(cadena);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("sp_GetProductos", connection);
+            dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure; 
+
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable); 
+
+            return dataTable;
+        }
+
+        private DataTable GetCategorias()
+        {
+            SqlConnection connection = new SqlConnection(cadena);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("sp_GetCategorias", connection);
+            dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            return dataTable;
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            cboCategoria.DataSource = GetCategorias();
+            cboCategoria.DisplayMember = "nombreCategoria";
+            cboCategoria.ValueMember = "idCategoria";
+
+            dgProducto.DataSource = GetProductos(); 
+
+        }
+
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(cadena);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("sp_InsertProducto", connection); 
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@prmstridCategoria", cboCategoria.SelectedValue);
+                cmd.Parameters.AddWithValue("@prmstrnombreProducto", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@prmstrprecioUnitario", txtPrecio.Text);
+                cmd.Parameters.AddWithValue("@prmintrStock", txtStock.Text);
+
+                connection.Open();
+                int registros = cmd.ExecuteNonQuery(); 
+                if( registros > 0)
+                {
+                    dgProducto.DataSource = GetProductos();
+                    MessageBox.Show("Se realizo el registro"); 
+                }
+                else
+                {
+                    MessageBox.Show("Error en el registro");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(cadena);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("sp_UpdateProducto ", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@prmstridProducto",txtId.Text);
+                cmd.Parameters.AddWithValue("@prmstridcategoria", cboCategoria.SelectedValue);
+                cmd.Parameters.AddWithValue("@prmstrnombreProducto", txtNombre.Text); 
+                cmd.Parameters.AddWithValue("@prmstrprecioUnitario ", txtPrecio.Text);
+                cmd.Parameters.AddWithValue("@prmintrStock", txtStock.Text);
+                connection.Open();
+                int registros = cmd.ExecuteNonQuery(); 
+
+                if( registros >0)
+                {
+                    dgProducto.DataSource = GetProductos();
+                    MessageBox.Show("Se actualizo el registro");
+                }
+                else
+                {
+                    MessageBox.Show("Ocurrio un error");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+
+        private void dgProducto_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow fila = dgProducto.CurrentRow;
+
+
+            txtId.Text = fila.Cells[0].Value.ToString();
+            cboCategoria.Text = fila.Cells[1].Value.ToString(); 
+            txtNombre.Text = fila.Cells[2].Value.ToString();
+            txtPrecio.Text = fila.Cells[3].Value.ToString();
+            txtStock.Text = fila.Cells[4].Value.ToString();      
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtId.Text))
+            {
+                MessageBox.Show("Seleccionar registro");
+
+                return; 
+            }
+
+            SqlConnection connection = new SqlConnection(cadena);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("sp_DeleteProducto", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@prmstridProducto", txtId.Text);
+
+                connection.Open();
+                int registros = cmd.ExecuteNonQuery();
+
+                if(registros > 0)
+                {
+                    dgProducto.DataSource = GetProductos();
+                    txtId.Text = "";
+                    txtNombre.Text = "";
+                    txtPrecio.Text = "";
+                    txtStock.Text = "";
+                    MessageBox.Show("Se elimino correctamente");
+                }
+                else
+                {
+                    MessageBox.Show("ocurrio un error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+               
+            }
+        }
+    }
+}
